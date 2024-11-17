2b37442e74be5aa562acd695191dc572fab0db51
HoangHuy <hoanghuya1thptcc@gmail.com>
2024-11-17 19:06:54 +0700

    Add project files.

---
+++ Harmic/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Harmic.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly HarmicContext _context;
+        public ProductController(HarmicContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/product/{alias}-{id}.html")]
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TbProducts == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.TbProducts.Where(m => (bool)m.IsActive).Where(m => (bool)m.IsNew)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+			var productReviews = _context.TbProductReviews
+	        .Where(m => m.ProductId == id && m.IsActive == true)
+        	.ToList() ?? new List<TbProductReview>();
+
+			ViewBag.productReviews = productReviews;
+
+			ViewBag.productRelated = _context.TbProducts
+                .Where(m => m.ProductId != id && m.CategoryProductId == product.CategoryProductId)
+                .OrderByDescending(m => m.ProductId).ToList();
+            return View(product);
+        }
+    } 
+}
---
+++ Harmic/Models/HarmicContext.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Harmic.Models;
+
+public partial class HarmicContext : DbContext
+{
+    public HarmicContext()
+    {
+    }
+
+    public HarmicContext(DbContextOptions<HarmicContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TbAccount> TbAccounts { get; set; }
+
+    public virtual DbSet<TbBlog> TbBlogs { get; set; }
+
+    public virtual DbSet<TbBlogComment> TbBlogComments { get; set; }
+
+    public virtual DbSet<TbCategory> TbCategories { get; set; }
+
+    public virtual DbSet<TbContact> TbContacts { get; set; }
+
+    public virtual DbSet<TbMenu> TbMenus { get; set; }
+
+    public virtual DbSet<TbNews> TbNews { get; set; }
+
+    public virtual DbSet<TbOrder> TbOrders { get; set; }
+
+    public virtual DbSet<TbOrderDetail> TbOrderDetails { get; set; }
+
+    public virtual DbSet<TbOrderStatus> TbOrderStatuses { get; set; }
+
+    public virtual DbSet<TbProduct> TbProducts { get; set; }
+
+    public virtual DbSet<TbProductCategory> TbProductCategories { get; set; }
+
+    public virtual DbSet<TbProductReview> TbProductReviews { get; set; }
+
+    public virtual DbSet<TbRole> TbRoles { get; set; }
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TbAccount>(entity =>
+        {
+            entity.HasKey(e => e.AccountId).HasName("PK__tb_Accou__349DA5A612E30E69");
+
+            entity.ToTable("tb_Account");
+
+            entity.Property(e => e.AccountId).ValueGeneratedNever();
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.FullName).HasMaxLength(255);
+            entity.Property(e => e.LastLogin).HasColumnType("datetime");
+            entity.Property(e => e.Password).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.Username).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<TbBlog>(entity =>
+        {
+            entity.HasKey(e => e.BlogId).HasName("PK__tb_Blog__54379E3005DED19E");
+
+            entity.ToTable("tb_Blog");
+
+            entity.Property(e => e.BlogId).ValueGeneratedNever();
+            entity.Property(e => e.Alias).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Image).HasMaxLength(255);
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.SeoTitle).HasMaxLength(255);
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<TbBlogComment>(entity =>
+        {
+            entity.HasKey(e => e.CommentId).HasName("PK__tb_BlogC__C3B4DFCAC6C6583D");
+
+            entity.ToTable("tb_BlogComment");
+
+            entity.Property(e => e.CommentId).ValueGeneratedNever();
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<TbCategory>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__tb_Categ__19093A0B92442985");
+
+            entity.ToTable("tb_Category");
+
+            entity.Property(e => e.CategoryId).ValueGeneratedNever();
+            entity.Property(e => e.Alias).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.SeoTitle).HasMaxLength(255);
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<TbContact>(entity =>
+        {
+            entity.HasKey(e => e.ContactId).HasName("PK__tb_Conta__5C66259BC25D5458");
+
+            entity.ToTable("tb_Contact");
+
+            entity.Property(e => e.ContactId).ValueGeneratedNever();
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<TbMenu>(entity =>
+        {
+            entity.HasKey(e => e.MenuId).HasName("PK__tb_Menu__C99ED23068FFF0C2");
+
+            entity.ToTable("tb_Menu");
+
+            entity.Property(e => e.Alias).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<TbNews>(entity =>
+        {
+            entity.HasKey(e => e.NewsId).HasName("PK__tb_News__954EBDF32FE43E91");
+
+            entity.ToTable("tb_News");
+
+            entity.Property(e => e.NewsId).ValueGeneratedNever();
+            entity.Property(e => e.Alias).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Image).HasMaxLength(255);
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.SeoTitle).HasMaxLength(255);
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<TbOrder>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__tb_Order__C3905BCF551EBAB0");
+
+            entity.ToTable("tb_Order");
+
+            entity.Property(e => e.OrderId).ValueGeneratedNever();
+            entity.Property(e => e.Address).HasMaxLength(255);
+            entity.Property(e => e.Code).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.CustomerName).HasMaxLength(255);
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.TotalAmount).HasColumnType("decimal(18, 2)");
+        });
+
+        modelBuilder.Entity<TbOrderDetail>(entity =>
+        {
+            entity.HasKey(e => e.OrderDetailId).HasName("PK__tb_Order__D3B9D36C0D2C1B1D");
+
+            entity.ToTable("tb_OrderDetail");
+
+            entity.Property(e => e.OrderDetailId).ValueGeneratedNever();
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+        });
+
+        modelBuilder.Entity<TbOrderStatus>(entity =>
+        {
+            entity.HasKey(e => e.OrderStatusId).HasName("PK__tb_Order__BC674CA153568FE6");
+
+            entity.ToTable("tb_OrderStatus");
+
+            entity.Property(e => e.OrderStatusId).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<TbProduct>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__tb_Produ__B40CC6CD2492BD64");
+
+            entity.ToTable("tb_Product");
+
+            entity.Property(e => e.Alias).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Image).HasMaxLength(255);
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.PriceSale).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<TbProductCategory>(entity =>
+        {
+            entity.HasKey(e => e.CategoryProductId).HasName("PK__tb_Produ__FAFA184F236C3BB6");
+
+            entity.ToTable("tb_ProductCategory");
+
+            entity.Property(e => e.CategoryProductId).ValueGeneratedNever();
+            entity.Property(e => e.Alias).HasMaxLength(255);
+            entity.Property(e => e.CreatedBy).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Icon).HasMaxLength(255);
+            entity.Property(e => e.ModifiedBy).HasMaxLength(255);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<TbProductReview>(entity =>
+        {
+            entity.HasKey(e => e.ProductReviewId).HasName("PK__tb_Produ__396318802161050B");
+
+            entity.ToTable("tb_ProductReview");
+
+            entity.Property(e => e.ProductReviewId).ValueGeneratedNever();
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<TbRole>(entity =>
+        {
+            entity.HasKey(e => e.RoleId).HasName("PK__tb_Role__8AFACE1A2828CA89");
+
+            entity.ToTable("tb_Role");
+
+            entity.Property(e => e.RoleId).ValueGeneratedNever();
+            entity.Property(e => e.RoleName).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
